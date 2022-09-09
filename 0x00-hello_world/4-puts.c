f2efefcff3ddff21c2304d5c23cf825ed20ac5da
nnamdi nwokenaka <nwokenakannamdi@gmail.com>
2022-09-08 17:44:33 -0700

    hw4c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main
- * Description: prints "\"Programming is like a multilingual puzzle"
+ * Description: prints "\"Programming is like building a multilingual puzzle"
  * Return 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
