8e7020a8b953e09319cf856cc3bc0b814be1b1f1
nnamdi nwokenaka <nwokenakannamdi@gmail.com>
2022-09-08 18:18:42 -0700

    hw5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry point;
  * Description: prints "\"Programming is like building a multilingual language"
- * Return 0
+ * Return 0;
  */
 
-int main(void)
-
+int main(void);
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
