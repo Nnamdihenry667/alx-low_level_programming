3b99f15f37cdccf45b53b4f7465faba801cfe365
nnamdi nwokenaka <nwokenakannamdi@gmail.com>
2022-09-10 03:18:13 -0700

    v5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Description: print decimal numbers 0-9
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i; i < 10; i++)
+	{
+
+		printf("%i", i);
+
+	}
+	return (0);
+}
