804af943fe1c9ab41c2bec4ab1b04e4a093271a0
nnamdi nwokenaka <nwokenakannamdi@gmail.com>
2022-09-10 03:46:45 -0700

    v6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Description: print all decimal single digits
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar('i');
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
