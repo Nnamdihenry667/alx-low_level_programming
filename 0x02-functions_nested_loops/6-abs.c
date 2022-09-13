df551d5b70faec1539d3bf25a82d466e6b3b7579
nnamdi nwokenaka <nwokenakannamdi@gmail.com>
2022-09-13 14:32:32 -0700

    nl6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - checks absolute value for an integer
+ * @n: checks the character
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
