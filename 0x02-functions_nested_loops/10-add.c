7bd33c1a30fdee57dd0d1fc7ba491d8526bcfa8e
nnamdi nwokenaka <nwokenakannamdi@gmail.com>
2022-09-14 09:57:48 -0700

    nl10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - returns the sum of two numbers
+ * Return: the sum
+ */
+
+int add(int c, int d)
+{
+	int a, b, sum;
+
+	sum = a + b;
+	_putchar(sum);
+	_putchar('\n');
+}
