bbd1bf99631dd242d15061d91adfbcf86ddd00a6
nnamdi nwokenaka <nwokenakannamdi@gmail.com>
2022-09-16 15:39:36 -0700

    mf9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @void: no argument included
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("Buzz");
+	printf("\n");
+
+	return (0);
+}
