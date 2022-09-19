4f0a7fe1c90ee1fe6947d23de0d3685a2728e3c6
nnamdi nwokenaka <nwokenakannamdi@gmail.com>
2022-09-19 13:45:49 -0700

    pa2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the length of a string
+ * @s: string to be checked
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		length++;
+	}
+}
