94147b3ca13e717562423af328f2d9237a1f5400
nnamdi nwokenaka <nwokenakannamdi@gmail.com>
2022-09-20 11:48:58 -0700

    pa9

---
+++ 0x05-pointers_arrays_strings/strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string pointed to by src
+ * @dest: buffer dest
+ * @src: string
+ * Return: value to the pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int index;
+	index = 0;
+
+	while (index >= 0)
+	{
+		dest[index] = src[index];
+
+		if (src[index] == '\0')
+		{
+			return (dest);
+		}
+		index++;
+	}
+	return (dest);
+}
