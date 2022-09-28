5250d8ea36f1cc6e14f66016b07703342dfe7286
nnamdi nwokenaka <nwokenakannamdi@gmail.com>
2022-09-28 13:44:02 -0700

    r4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculates the value of an integer raised to a power
+ * @x: base
+ * @y: power
+ * Return: the value of the calculated integer
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y <= 0)
+	{
+		return (x);
+	}
+	else
+	{
+		return (x * (_pow_recursion(x, y - 1)));
+	}
+}
