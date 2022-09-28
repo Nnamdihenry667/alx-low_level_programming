920d1dbff23b5d0f8d01e6393b310a28df89ec6c
nnamdi nwokenaka <nwokenakannamdi@gmail.com>
2022-09-28 12:46:24 -0700

    r2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - counts the length of a string using recursion
+ * @s: pointer to the string
+ * Return: the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_strlen_recursion(s + 1);
+		count++;
+	}
+	return (count);
+}
