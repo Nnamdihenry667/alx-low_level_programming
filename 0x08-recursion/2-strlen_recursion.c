f7d0c768e28186b2d30b0114e79af1edf7d2a4ae
nnamdi nwokenaka <nwokenakannamdi@gmail.com>
2022-09-28 13:07:34 -0700

    r2b

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,10 +10,13 @@ int _strlen_recursion(char *s)
 {
 	int count = 0;
 
-	if (*s != '\0')
+	if (*s == '\0')
+	{
+		return (count);
+	}
+	else
 	{
 		_strlen_recursion(s + 1);
 		count++;
 	}
-	return (count);
 }
