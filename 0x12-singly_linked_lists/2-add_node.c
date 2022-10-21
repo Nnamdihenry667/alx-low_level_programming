4d404b889465497fa598df34f5f4a5c10eec9670
nwokenaka nnamdi <nwokenakannamdi@gmail.com>
2022-10-21 04:07:13 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,30 +2,20 @@
 
 #include <string.h>
 
-#include <stdio.h>
-
 /**
- * add_node_end - add a node at the end of a linked list
- * @head: The character to print
- * @str: string for the new node
- * Return: new node
+ * add_node - adds a node to a linked list head
+ * @head: linked list head
+ * @str: string element of the linked list
+ * Return: number of elements
  */
 
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
-	list_t *current;
-
 	list_t *new_node;
 
-	int c;
+	int len_str;
 
-	current = *head;
-
-	while (current && current->next != NULL)
-		current = current->next;
-	for (c = 0; str[c] != '\0'; c++)
-		;
-	new_node = malloc(sizeof(list_t));
+	new_node = (list_t *) malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
 		free(new_node);
@@ -37,11 +27,10 @@ list_t *add_node_end(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = c;
-	new_node->next = NULL;
-	if (current)
-		current->next = new_node;
-	else
-		*head = new_node;
+	for (len_str = 0; str[len_str] != '\0';  len_str++)
+		;
+	new_node->len = len_str;
+	new_node->next = *head;
+	*head = new_node;
 	return (new_node);
 }
